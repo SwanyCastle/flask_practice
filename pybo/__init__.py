ba721eaca1db0d1cc274d5cc7572c7602b0f44f7
SwanyCastle <seunghwan712@gmail.com>
2023-12-25 13:38:48 +0900

    static 설정

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -19,7 +19,7 @@ migrate = Migrate()
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static', static_url_path='/static')
     app.config.from_envvar('APP_CONFIG_FILE')
 
     # ORM
